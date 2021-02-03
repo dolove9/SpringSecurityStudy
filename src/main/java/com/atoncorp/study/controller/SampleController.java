88b38d719992f9979f5e80b039cd2c71827fc58c
jclee@atoncorp.com <jclee@atoncorp.com>
2021-02-03 21:42:56 +0900

    첫샘플 작성
    Principal 객체 추가

--- src/main/java/com/atoncorp/study/controller/SampleController.java
+++ src/main/java/com/atoncorp/study/controller/SampleController.java
@@ -4,12 +4,36 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.security.Principal;
+
 @Controller
 public class SampleController {
 
     @GetMapping("/")
-    public String index(Model model){
-        model.addAttribute("message", "Hello SpringSecurity");
+    public String index(Model model, Principal principal){
+        if(principal == null){
+            model.addAttribute("message", "Hello SpringSecurity");
+        }else{
+            model.addAttribute("message", "Hello SpringSecurity " + principal.getName());
+        }
         return "index";
+    }
+
+    @GetMapping("/info")
+    public String info(Model model){
+        model.addAttribute("message", "Hello Information Page");
+        return "info";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard(Model model, Principal principal){
+        model.addAttribute("message", "Hello " + principal.getName());
+        return "dashboard";
+    }
+
+    @GetMapping("/admin")
+    public String admin(Model model, Principal principal){
+        model.addAttribute("message", "Hello Admin " +  principal.getName());
+        return "admin";
     }
 }
